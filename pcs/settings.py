cdacc91049b92a30a24022276d5c87796910cacf
Claudiu Popescu <claudiu.popescu@camatweb.com>
2014-05-22 13:00:42 +0300

    Lib path based on OS

--- pcs/settings.py
+++ pcs/settings.py
@@ -1,11 +1,18 @@
+from platform import linux_distribution
+
+if 'Ubuntu' or 'Debian' in linux_distribution():
+    base_lib_path = "/usr/lib"
+else:
+    base_lib_path = "/usr/libexec"
+
 pacemaker_binaries = "/usr/sbin/"
 corosync_binaries = "/usr/sbin/"
 corosync_conf_file = "/etc/corosync/corosync.conf"
 cluster_conf_file = "/etc/cluster/cluster.conf"
 fence_agent_binaries = "/usr/sbin/"
-pengine_binary = "/usr/libexec/pacemaker/pengine"
-crmd_binary = "/usr/libexec/pacemaker/crmd"
-stonithd_binary = "/usr/libexec/pacemaker/stonithd"
+pengine_binary = base_lib_path + "/pacemaker/pengine"
+crmd_binary = base_lib_path + "/pacemaker/crmd"
+stonithd_binary = base_lib_path + "/pacemaker/stonithd"
 pcs_version = "0.9.115"
 crm_report = pacemaker_binaries + "crm_report"
 crm_verify = pacemaker_binaries + "crm_verify"
